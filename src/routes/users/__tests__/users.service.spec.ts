ab403121e0e4e4aecd930e34f00ca46e60e6dba3
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2025-01-17 16:16:45 +0100

    fix: import errors

--- src/routes/users/__tests__/users.service.spec.ts
+++ src/routes/users/__tests__/users.service.spec.ts
@@ -1,27 +1,31 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersRepository } from '../users.repository';
+import { compare } from 'bcrypt';
 import { UsersService } from '../users.service';
-import { compare, hash } from 'bcrypt';
 
 describe('UsersService', () => {
-    let usersService: UsersService;
-    let userRepository;
+  let usersService: UsersService;
+  let userRepository;
 
-    beforeEach(async () => {
-        userRepository = {
-            addUser: jest.fn()
-        };
+  beforeEach(async () => {
+    userRepository = {
+      addUser: jest.fn(),
+    };
 
-        usersService = new UsersService(userRepository);
-    });
+    usersService = new UsersService(userRepository);
+  });
 
-    it('should register user with the register repository', async () => {
-        const password = 'testPassword';
-        await usersService.register('testUser', password);
+  it('should register user with the register repository', async () => {
+    const password = 'testPassword';
+    await usersService.register('testUser', password);
 
-        expect(userRepository.addUser).toHaveBeenCalledWith('testUser', expect.any(String));
+    expect(userRepository.addUser).toHaveBeenCalledWith(
+      'testUser',
+      expect.any(String),
+    );
 
-        const match = await compare(password, userRepository.addUser.mock.calls[0][1]);
-        expect(match).toBeTruthy();
-    });
+    const match = await compare(
+      password,
+      userRepository.addUser.mock.calls[0][1],
+    );
+    expect(match).toBeTruthy();
+  });
 });
